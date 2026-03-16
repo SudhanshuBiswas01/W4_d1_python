8fa406ae1c0f1f2f5bc3aeaffd9b8bac39cafe68
SudhanshuBiswas01 <sudhanshubiswas2003@gmail.com>
2026-03-16 11:00:21 +0530

    6th part 2nd activity

--- 6-conditional-statements.py
+++ 6-conditional-statements.py
@@ -4,3 +4,12 @@ if age >= 18:
     print("Adult")
 else:
     print("Miner")
+
+# 2nd activity 
+
+num = int(input("Enter a number :"))
+
+if num > 0:
+    print(f"{num} is positive")
+elif num < 0:
+    print(f" {num} isNegative")
