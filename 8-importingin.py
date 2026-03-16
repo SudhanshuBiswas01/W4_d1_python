059401c39a8416d39ddf93dfa09d6132093b424d
SudhanshuBiswas01 <sudhanshubiswas2003@gmail.com>
2026-03-16 11:56:03 +0530

    8thpart

---
+++ 8-importingin.py
@@ -0,0 +1,17 @@
+#--------------------------------#
+
+import math
+print(math.sqrt(100))
+
+#--------------------------------#
+
+import random 
+print(random.randint(1, 100))
+
+#--------------------------------#
+
+import datetime
+print(datetime.datetime.now())
+
+#--------------------------------#
+
